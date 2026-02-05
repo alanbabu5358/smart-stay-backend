0a43a095f6141c285cfbf10ea7574da7698c53a0
alanbabu3774 <157644521+alanbabu3774@users.noreply.github.com>
2026-02-05 03:45:30 -0500

    Sprint 1 backend setup completed

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('Smart Stay Backend is running');
+});
+
+// User routes (FIXED PATH)
+const userRoutes = require("./routes/users");
+app.use("/api/users", userRoutes);
+
+// Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
